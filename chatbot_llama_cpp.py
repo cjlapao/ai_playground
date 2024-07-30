79776c2c6a3fdaae42e80b7538442416b81dd6c2
Carlos <cjlapao@gmail.com>
2024-07-30 13:49:54 +0100

    added face recognition

--- chatbot_llama_cpp.py
+++ chatbot_llama_cpp.py
@@ -1,15 +1,20 @@
 from huggingface_hub import hf_hub_download
+from huggingface_hub import login
 from llama_cpp import Llama
 
+login()
 ## Download the GGUF model
-model_name = "microsoft/Phi-3-mini-4k-instruct-gguf"
-model_file = "Phi-3-mini-4k-instruct-q4.gguf"
+# model_name = "microsoft/Phi-3-mini-4k-instruct-gguf"
+# model_file = "Phi-3-mini-4k-instruct-q4.gguf"
 
 # model_name = "matteocavestri/Mixtral-8x7B-Instruct-v0.1-Q4_K_M-GGUF"
 # model_file = "mixtral-8x7b-instruct-v0.1-q4_k_m.gguf"
 
 # model_name = "TheBloke/Llama-2-7b-Chat-GGUF"
 # model_file = "llama-2-7b-chat.Q8_0.gguf"
+
+model_name = "TheBloke/OpenHermes-2.5-Mistral-7B-GGUF"
+model_file = "openhermes-2.5-mistral-7b.Q4_K_M.gguf"
 
 model_path = hf_hub_download(model_name, filename=model_file)
 
